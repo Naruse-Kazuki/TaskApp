d138ad926e75b6f3ca43ea224613e443fd262774
EC2 Default User <ec2-user@ip-172-31-44-106.ap-northeast-1.compute.internal>
2019-09-25 14:29:35 +0000

    c7

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,40 @@
 class TasksController < ApplicationController
   
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = '新規登録に成功しました。'
+      redirect_to @task
+    else
+      render :new
+    end
+  end
+  
   def index
     @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    if @task.save
+      flash[:success] = 'タスク編集に成功しました。'
+      redirect_to @task
+    else
+      render :edit
+    end
   end
   
 end
